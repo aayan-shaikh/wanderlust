ef0146d197fd0dcf7795776a849c87ebb27e4f27
Aayan Shaikh <82713836+aayan-shaikh@users.noreply.github.com>
2025-07-24 16:29:41 +0530

    Fix mobile blending mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import { imageService } from "@unpic/astro/service";
 export default defineConfig({
   base: "/",
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+  },
 });
